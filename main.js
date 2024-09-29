ac28af61b6f224c74057e40b44fd053d6a4eb9bd
Taiho1998 <tgo12323@gmail.com>
2024-09-29 09:57:05 +0900

    기본 발생 이벤트 비활성화 후 다중 선택이 가능하게 함.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 "use strict";
 
 function Product(name, price){
-    this.name = name
-    this.price = price
+    this.name = name;
+    this.price = price;
 }
 let products = [
     new Product('대뱃살', 3000),
@@ -10,3 +10,16 @@ let products = [
     new Product('배꼽살', 4000),
     new Product('중뱃살', 1000)
 ]
+
+let prodList = document.getElementById('prodList')
+prodList.addEventListener('mousedown', function(e){
+    e.preventDefault();
+    let option = e.target
+    if (option.selected){
+        option.selected = false;
+    }else {
+        option.selected = true;
+    }
+
+    console.log(option.selected, option)
+})
