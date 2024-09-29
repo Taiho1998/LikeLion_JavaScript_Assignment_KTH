8b187875bf108a3ec28e892e56d8577b82f837f5
Taiho1998 <tgo12323@gmail.com>
2024-09-29 16:42:07 +0900

    purchase.html로 부터 카드 번호 입력후 부모 창의 함수 출력. 기본적으로 구현해야할 기능 구현 완료

--- main.js
+++ main.js
@@ -12,12 +12,15 @@ let products = [
 ]
 
 
-let prodList = document.getElementById('prodList')
-let resultNode = document.getElementById('result')
-let buttonNode = document.getElementById('submit')
+let prodList = document.getElementById('prodList');
+let resultNode = document.getElementById('result');
+let buttonNode = document.getElementById('submit');
+
 for(let i = 0; i < products.length; i++)
-    prodList.innerHTML += `<option onclick="addList()" class="product" value="${i}">${products[i].name} - ${products[i].price}</option>`
-let itemNode = document.getElementsByClassName('product')
+    prodList.innerHTML += `<option onclick="addList()" class="product" value="${i}">${products[i].name} - ${products[i].price}</option>`;
+
+let itemNode = document.getElementsByClassName('product');
+
 // 선택된 항목이 저장되는 배열
 let selectedList = [];
 let totalPrice;
@@ -37,39 +40,57 @@ function addList() {
     totalPrice = 0;
     for (let i = 0; i < products.length; i++){
         if (itemNode[i].selected == true) {
-            selectedList.push(itemNode[i].value)
-            totalPrice += products[itemNode[i].value].price
+            selectedList.push(itemNode[i].value);
+            totalPrice += products[itemNode[i].value].price;
 
         }
     }
-    console.log(totalPrice)
-
-    // 선택한 항목이 존재하는 경우에만 발생
-        printList()
+    printList()
 }
 
+// 항목을 선택하면 리스트와 가격을 출력, 없으면 지우기
 function printList(){
+    // 선택한 항목이 존재하는 경우에만 발생
     if(selectedList.length != 0) {
         resultNode.innerHTML = '<h3>선택한 상품</h3><ul id = "resultLists"></ul>'
         for (let i = 0; i < selectedList.length; i++){
-            let resultLists = document.getElementById('resultLists')
-            let listNode = document.createElement('li')
-            let listNodeText = document.createTextNode(`${products[selectedList[i]].name} - ${products[selectedList[i]].price}`)
-            listNode.appendChild(listNodeText)
-            resultLists.appendChild(listNode)
+            let resultLists = document.getElementById('resultLists');
+            let listNode = document.createElement('li');
+            let listNodeText = document.createTextNode(`${products[selectedList[i]].name} - ${products[selectedList[i]].price}`);
+            listNode.appendChild(listNodeText);
+            resultLists.appendChild(listNode);
         }
         // hidden 속성 input은 결제 페이지로 totalprice를 넘기기 위한 더미 요소
-        resultNode.innerHTML += `<h3>총액 : ${totalPrice}</h3><input type="hidden" id="totalPrice" value="${totalPrice}">`
-    }else {
-        resultNode.innerHTML = null
+        resultNode.innerHTML += `<h3>총액 : ${totalPrice}</h3><input type="hidden" id="totalPrice" value="${totalPrice}">`;
+    }else { //기본값은 null
+        resultNode.innerHTML = null;
     }
+    console.log(selectedList, totalPrice)
 }
 
+// 결제하기 버튼 이벤트
 buttonNode.addEventListener('click', function(){
     // 선택된 항목이 없을 시 오류 메시지 출력
     if(!selectedList.length){
-        alert('결제할 상품을 선택해야 합니다.')
+        alert('결제할 상품을 선택해야 합니다.');
         return;
     }
-    window.open('purchase.html', '_blank', 'left=100, top = 100, width = 500, height = 400')
+    window.open('purchase.html', '_blank', 'left=100, top = 100, width = 500, height = 400');
 })
+
+// 결제 창에서 결제 완료시 실행할 함수
+function clearData(){
+    for (let i = 0; i < products.length; i++){
+        if (itemNode[i].selected)
+            itemNode[i].selected = !itemNode[i].selected;
+    }
+    selectedList = [];
+    totalPrice = 0;
+    // selectedList 데이터를 초기화한 다음 이미 존재하는 printList() 함수를 통해 결과창 삭제
+    printList();
+}
+
+// 결제창으로부터 카드 번호 데이터가 전송되었는지 확인하는 더미 함수
+function sendCardData(cardNo){
+    // console.log(cardNo)
+}
